1d853f3ad4f28e7cb1d3fcea3e1fa81951865faf
Marine G <marine.guichon@horanet.com>
2022-11-25 14:52:15 +0100

    [13.0][password_security] : Delete old meter to display translating meter

--- password_security/static/src/js/signup_policy.js
+++ password_security/static/src/js/signup_policy.js
@@ -2,21 +2,24 @@ odoo.define("password_security.signup_policy", function(require) {
     "use strict";
 
     require("web.dom_ready");
+    require("auth_password_policy_signup.policy");
     var policy = require("auth_password_policy");
     var PasswordMeter = require("password_security.Meter");
 
     var $signupForm = $(".oe_signup_form, .oe_reset_password_form");
+
     if (!$signupForm.length) {
         return;
     }
+    $signupForm.find(".field-password meter").remove();
 
     var $password = $signupForm.find("#password");
-    var password_length = Number($password.attr("password_length"));
-    var password_lower = Number($password.attr("password_lower"));
-    var password_upper = Number($password.attr("password_upper"));
-    var password_numeric = Number($password.attr("password_numeric"));
-    var password_special = Number($password.attr("password_special"));
-    var password_estimate = Number($password.attr("password_estimate"));
+    var password_length = $password.data("password_length");
+    var password_lower = $password.data("password_lower");
+    var password_upper = $password.data("password_upper");
+    var password_numeric = $password.data("password_numeric");
+    var password_special = $password.data("password_special");
+    var password_estimate = $password.data("password_estimate");
 
     var meter = new PasswordMeter(
         null,
